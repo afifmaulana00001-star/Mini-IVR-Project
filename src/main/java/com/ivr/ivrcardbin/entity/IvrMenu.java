e25ad2643f8a7b3f0709c7289187d6af022ca51c
afifmaulana <afifmaulana00001@gmail.com>
2026-03-12 15:07:42 +0700

    Tambahan System Pin Verivikasi

---
+++ src/main/java/com/ivr/ivrcardbin/entity/IvrMenu.java
@@ -0,0 +1,4 @@
+package com.ivr.ivrcardbin.entity;
+
+public class IvrMenu {
+}
--- src/main/java/com/ivr/ivrcardbin/utils/LuhnValidator.java
+++ src/main/java/com/ivr/ivrcardbin/utils/LuhnValidator.java
@@ -1,4 +1,28 @@
 package com.ivr.ivrcardbin.utils;
 
 public class LuhnValidator {
+
+    public static boolean isValid(String cardNumber){
+        int sum = 0;
+        boolean alternate = false;
+
+        for (int i =cardNumber.length() -1; i>=0; i--){
+
+            int n = Integer.parseInt(cardNumber.substring(i,i + i));
+
+            if (alternate){
+                n *= 2;
+                if (n>9){
+                    n = (n % 10)+1;
+
+                }
+
+        }
+            sum += n;
+            alternate = !alternate;
+
+        }
+        return (sum % 10 == 0);
+    }
+
 }
